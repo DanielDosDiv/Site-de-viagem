c5551a845bd10a4401cafd6233c97a10372b03f5
Daniel dos Santos de Souza <danieldesouza1024@gamil.com>
2023-05-30 17:13:13 -0300

    Atualização do site de viagem mobile

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 let btnGet = document.querySelector(".btnGet");
+let login = document.querySelector(".BTN-SignIn");
+let tetarNovamente = document.querySelector(".ok")
+let opacidadeFundo = document.querySelector(".filter-opacidade");
+let msgErro = document.querySelector(".mensagem-Erro");
+login.addEventListener("click" , () =>{
+    let password = document.getElementById("password").value;
+    let user = document.getElementById("user").value;
+    
+
+    if (user == "user@gmail.com" && password == "user123"){
+        window.location.href = 'home.html'
+    }
+    else{
+        msgErro.classList.add("ativo")
+        opacidadeFundo.classList.add("ativo")
+    }
+})
+tetarNovamente.addEventListener("click", () => {
+    msgErro.classList.remove("ativo")
+    opacidadeFundo.classList.remove("ativo")
+})
 
 btnGet.addEventListener("click" , () =>{
     let span1 = document.querySelector(".span1");
