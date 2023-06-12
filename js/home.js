6a3a6c9a1415fc3b6f0917d195e1eeaccc2cbe8c
Daniel dos Santos de Souza <danieldesouza1024@gmail.com>
2023-06-12 19:58:23 -0300

    Cmeçando Menu lateral

--- js/home.js
+++ js/home.js
@@ -1,7 +1,34 @@
 let escolherViagem = document.querySelector(".escolher-viagem");
     escolherViagem.addEventListener("click", () => {
         let conteinerViagem1 = document.querySelector(".conteinerViagem1");
-        conteinerViagem1.classList.toggle("selecionado")
+        conteinerViagem1.classList.add("selecionado")
         console.log("Chegei")
 
     })
+// Fazer menu lateral apareçer
+
+let iconMenu = document.querySelector(".menu");
+iconMenu.addEventListener("click" , () =>{
+    let menuLateral = document.querySelector(".menu-lateral");
+    menuLateral.classList.toggle("active")
+    if (iconMenu.classList.contains("fa-bars")) {
+        iconMenu.classList.remove("fa-bars")
+        iconMenu.classList.add("fa-xmark")
+    }
+    else{
+        iconMenu.classList.remove("fa-xmark")
+        iconMenu.classList.add("fa-bars")
+    }
+})
+let mudarTema = document.querySelector(".mudar-tema");
+mudarTema.addEventListener("click" ,() => {
+    let btnMudarTema = document.querySelector("span");
+    let lua = document.querySelector(".fa-moon");
+    btnMudarTema.classList.toggle("ativado")
+    mudarTema.classList.toggle("ativado")
+    
+    lua.classList.toggle("fa-sun")
+
+    let themaDark = document.querySelectorAll("")
+    
+})
